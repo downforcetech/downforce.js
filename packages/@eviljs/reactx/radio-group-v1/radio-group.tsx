63d64d335059162484b54594ae4f093118e30537
Daniele Orlando <dnl.rlnd@gmail.com>
2025-03-11 13:26:44 +0100

    (@eviljs/reactx) reworks RadioGroup customization

--- packages/@eviljs/reactx/radio-group-v1/radio-group.tsx
+++ packages/@eviljs/reactx/radio-group-v1/radio-group.tsx
@@ -1,5 +1,6 @@
 import {classes} from '@eviljs/react/classes'
 import type {ElementProps, Props} from '@eviljs/react/props'
+import {compute, type Computable} from '@eviljs/std/fn-compute'
 import {useId} from 'react'
 
 export function RadioGroup(props: Props<RadioGroupProps>): React.JSX.Element {
@@ -11,27 +12,37 @@ export function RadioGroup(props: Props<RadioGroupProps>): React.JSX.Element {
             {...otherProps}
             className={classes('RadioGroup-e37b', className)}
         >
-            {items?.map((it, idx) =>
-                <label
-                    key={idx}
-                    className="item-bf74 std-flex std-flex-align-center"
-                    data-selected={selected === it.value}
-                >
-                    <input
-                        {...it.inputsProps}
-                        className={classes('radio-1370', it.inputsProps?.className)}
-                        type="radio"
-                        name={`radio-group-${id}`}
-                        value={it.value}
-                        checked={selected === it.value}
-                        // readOnly={true}
-                        onChange={(event) => onChange?.(event.target.value, idx)}
-                    />
-                    <span className="label-b0ad">
-                        {it.label}
-                    </span>
-                </label>
-            )}
+            {items?.map((it, idx) => {
+                const labelProps = compute(it.labelProps, it.value, idx)
+                const inputProps = compute(it.inputProps, it.value, idx)
+                const spanProps = compute(it.spanProps, it.value, idx)
+
+                return (
+                    <label
+                        {...labelProps}
+                        key={idx}
+                        className={classes('item-bf74', labelProps?.className)}
+                        data-selected={selected === it.value}
+                    >
+                        <input
+                            {...inputProps}
+                            className={classes('radio-1370', inputProps?.className)}
+                            type="radio"
+                            name={`radio-group-${id}-${idx}`}
+                            value={it.value}
+                            checked={selected === it.value}
+                            // readOnly={true}
+                            onChange={event => onChange?.(event.target.value, idx)}
+                        />
+                        <span
+                            {...spanProps}
+                            className={classes('label-b0ad', spanProps?.className)}
+                        >
+                            {it.label}
+                        </span>
+                    </label>
+                )
+            })}
         </div>
     )
 }
@@ -41,9 +52,11 @@ export function RadioGroup(props: Props<RadioGroupProps>): React.JSX.Element {
 export interface RadioGroupProps extends Omit<ElementProps<'div'>, 'onChange'> {
     selected?: undefined | null | string
     items?: undefined | null | Array<{
-        value: string
+        inputProps?: undefined | Computable<ElementProps<'input'>, [string, number]>
         label: React.ReactNode
-        inputsProps?: undefined | ElementProps<'input'>
+        labelProps?: undefined | Computable<ElementProps<'label'>, [string, number]>
+        value: string
+        spanProps?: undefined | Computable<ElementProps<'span'>, [string, number]>
     }>
     onChange?: undefined | ((value: string, idx: number) => void)
 }
