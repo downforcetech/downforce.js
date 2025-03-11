725f5e45b3e4639eda8b6403e3ce865e159c6e37
Daniele Orlando <dnl.rlnd@gmail.com>
2025-03-11 13:31:12 +0100

    (@eviljs/web) updates package urls

--- packages/@eviljs/web/ts/route-path.ts
+++ packages/@eviljs/web/ts/route-path.ts
@@ -1,6 +1,6 @@
 import {compute} from '@eviljs/std/fn-compute'
 import type {FnArgs, Io} from '@eviljs/std/fn-type'
-import {defineRouteParams, type RouteParamOptions, type RouteParamsDefinition} from './route.js'
+import {defineRouteParams, type RouteParamOptions, type RouteParamsDefinition} from './route-param.js'
 
 export function defineRoutePath<const R extends string, A extends FnArgs>(
     route: R,
