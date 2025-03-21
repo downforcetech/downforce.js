286e62b71d8ee3811df10cea231fa53a0e428b1e
Daniele Orlando <dnl.rlnd@gmail.com>
2025-03-21 13:27:32 +0100

    (playground-react) chore

--- playground-react/src/theme/theme-apis.ts
+++ playground-react/src/theme/theme-apis.ts
@@ -7,11 +7,9 @@ export function themeClassesOf(theme: Theme): Array<string> {
     return ['std-root', `std-theme-${theme}`, 'std-text', 'std-color-fg1', 'std-background-bg1']
 }
 
-export function whenTheme<D, L>(theme: Theme, matches: {dark: D, light: L}): D | L {
+export function mapTheme<D, L>(theme: Theme, matches: {dark: D, light: L}): D | L {
     switch (theme) {
-        case Theme.Dark:
-            return matches.dark
-        case Theme.Light:
-            return matches.light
+        case Theme.Dark: return matches.dark
+        case Theme.Light: return matches.light
     }
 }
