1e38f59fe5ae6ba457a2bf216eff3bb6e66a276c
Daniele Orlando <dnl.rlnd@gmail.com>
2025-06-04 20:53:47 +0200

    (@downforce/std) fixes enum strict typing

--- @downforce/std/ts/enum/enum-strict.ts
+++ @downforce/std/ts/enum/enum-strict.ts
@@ -1,6 +1,8 @@
 import type {None} from '../optional.js'
 import {trustEnum} from './enum-trust.js'
 
-export function strictEnum<E extends boolean | number | string, V extends E>(value: None | V, enumValues: Array<E>): undefined | V {
-    return trustEnum(value, enumValues) as undefined | V
+export function strictEnum<V extends E, E>(value: None | V, enumValues: Array<E>): undefined | V
+export function strictEnum<V, E extends V>(value: V, enumValues: Array<E>): undefined | E
+export function strictEnum(value: unknown, enumValues: Array<unknown>) {
+    return trustEnum(value, enumValues)
 }
--- @downforce/std/ts/enum/enum-trust.ts
+++ @downforce/std/ts/enum/enum-trust.ts
@@ -1,4 +1,4 @@
-export function trustEnum<E extends boolean | number | string>(value: unknown, enumValues: Array<E>): undefined | E {
+export function trustEnum<E>(value: unknown, enumValues: Array<E>): undefined | E {
     if (enumValues.includes(value as E)) {
         return value as E
     }
