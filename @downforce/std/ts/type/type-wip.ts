e2bed137ea81438031abffaab1d42e40b319f637
Daniele Orlando <dnl.rlnd@gmail.com>
2025-06-04 16:12:10 +0200

    (@downforce/std) adds wip types

---
+++ @downforce/std/ts/type/type-wip.ts
@@ -0,0 +1,3 @@
+export type FIX<V = unknown, description extends string = string> = V
+export type FIXME<V = unknown, description extends string = string> = V
+export type TODO<V = unknown, description extends string = string> = V
