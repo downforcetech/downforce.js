4cea6d1e714da052557a8453dd86766130286dc2
Daniele Orlando <dnl.rlnd@gmail.com>
2025-08-26 20:42:51 +0200

    [@downforce/web] response error provides apis for discriminating the error

--- @downforce/web/ts/response/response-error.ts
+++ @downforce/web/ts/response/response-error.ts
@@ -1,7 +1,34 @@
+import {isArray} from '@downforce/std/array'
 import {decodeResponseBody} from './response-body.js'
 
+export const ResponseErrorMonadTag = '@downforce/web/response/error'
+
+export class ResponseErrorKit {
+    static new(status: number, body: unknown): ResponseErrorMonad {
+        return createResponseError(status, body)
+    }
+
+    static isError(value: unknown): value is ResponseError {
+        return isResponseError(value)
+    }
+}
+
+export function isResponseError(value: unknown): value is ResponseError {
+    if (! isArray(value)) {
+        return false
+    }
+
+    const [first, second, monadTag] = value
+
+    return monadTag === ResponseErrorMonadTag
+}
+
+export function createResponseError(status: number, body: unknown): ResponseErrorMonad {
+    return [status, body, ResponseErrorMonadTag]
+}
+
 /**
-* @throws Response
+* @throws ResponseError
 **/
 export async function rejectResponseWhenError(responsePromise: Response | Promise<Response>): Promise<Response> {
     const response = await responsePromise
@@ -10,7 +37,7 @@ export async function rejectResponseWhenError(responsePromise: Response | Promis
         return throwResponseWhenError(response)
     }
     catch {
-        throw [response.status, await decodeResponseBody(response)]
+        throw createResponseError(response.status, await decodeResponseBody(response))
     }
 }
 
@@ -23,3 +50,8 @@ export function throwResponseWhenError(response: Response): Response {
     }
     return response
 }
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export type ResponseError = [status: number, body: unknown]
+export type ResponseErrorMonad = [...ResponseError, monadTag: typeof ResponseErrorMonadTag]
