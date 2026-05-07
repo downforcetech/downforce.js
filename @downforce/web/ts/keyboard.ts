6383e9ec0b29972ef74ef7a49850b649ac719a61
Daniele Orlando <dnl.rlnd@gmail.com>
2026-05-07 22:12:03 +0200

    [@downforce/web] aligns modules name convention [chore]

---
+++ @downforce/web/ts/keyboard.ts
@@ -0,0 +1,29 @@
+import {Enum} from '@downforce/std/enum'
+
+export const KeyboardKey: {
+    Alt: 'Alt'
+    ArrowDown: 'ArrowDown'
+    ArrowLeft: 'ArrowLeft'
+    ArrowRight: 'ArrowRight'
+    ArrowUp: 'ArrowUp'
+    Backspace: 'Backspace'
+    Control: 'Control'
+    Enter: 'Enter'
+    Escape: 'Escape'
+    Meta: 'Meta'
+    Shift: 'Shift'
+    Space: ' '
+} = Enum({
+    Alt: 'Alt',
+    ArrowDown: 'ArrowDown',
+    ArrowLeft: 'ArrowLeft',
+    ArrowRight: 'ArrowRight',
+    ArrowUp: 'ArrowUp',
+    Backspace: 'Backspace',
+    Control: 'Control',
+    Enter: 'Enter',
+    Escape: 'Escape',
+    Meta: 'Meta',
+    Shift: 'Shift',
+    Space: ' ',
+})
